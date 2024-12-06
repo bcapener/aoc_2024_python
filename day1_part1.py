58124c9ba336ec786b44893e5f38866f8bcb1a05
Brandon Capener <bcapener@gmail.com>
2024-12-05 20:16:15 -0700

    added day1_part2

--- day1_part1.py
+++ day1_part1.py
@@ -2,11 +2,14 @@
 import sys
 from pathlib import Path
 
-
-def main(path: Path):
+def read_data(path: Path):
     input_data_lines = path.read_text().splitlines()
     input_data_str_pairs = (l.split(None, maxsplit=1) for l in input_data_lines)
     input_data_int_pairs = ((int(l[0]), int(l[1])) for l in input_data_str_pairs)
+    return input_data_int_pairs
+
+def main(path: Path):
+    input_data_int_pairs = read_data(path)
     input_data_int_pairs_sorted = zip(*(sorted(list(t)) for t in zip(*input_data_int_pairs)))
     pair_distances = (abs(r-l) for l, r in input_data_int_pairs_sorted)
     total_distance = sum(pair_distances)
@@ -16,6 +19,6 @@ def main(path: Path):
 
 
 if __name__ == "__main__":
-    input_file = Path(__file__).resolve().parent / "day1_part1_input.txt"
+    input_file = Path(__file__).resolve().parent / "day1_input.txt"
     assert input_file.exists()
     sys.exit(main(input_file))
---
+++ day1_part2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+from collections import Counter, defaultdict
+from pathlib import Path
+from day1_part1 import read_data
+
+
+
+
+def main(path: Path):
+    input_data_int_pairs = read_data(path)
+
+    left, right = zip(*input_data_int_pairs)
+    right_count = dict(Counter(right))
+
+    similarity_score = sum(n * right_count.get(n, 0) for n in left)
+
+    print(f"{similarity_score=}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    input_file = Path(__file__).resolve().parent / "day1_input.txt"
+    assert input_file.exists()
+    sys.exit(main(input_file))
