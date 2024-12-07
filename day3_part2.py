4e29f0483d3b609e5e8d0a085b0dddbd97b2540d
Brandon Capener <bcapener@gmail.com>
2024-12-07 13:20:12 -0700

    refactor day 3 part 2 and added assert to check result

--- day3_part2.py
+++ day3_part2.py
@@ -6,19 +6,23 @@ from pathlib import Path
 mul_instruction_pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
 
 def main(path: Path):
-    data = path.read_text()
-    data = "do()" + data
+    data = "do()" + path.read_text()
+
     result = 0
-    for d in data.split("don't()"):
-        if "do()" not in d:
+    for donts in data.split("don't()"):
+        try:
+            _, dos = donts.split("do()", maxsplit=1)
+        except ValueError:
+            # no do() in text
             continue
-        dos = d.split("do()", maxsplit=1)
-        for multiplicand_str, multiplicator_str in re.findall(mul_instruction_pattern, dos[-1]):
+
+        for multiplicand_str, multiplicator_str in re.findall(mul_instruction_pattern, dos):
             multiplicand = int(multiplicand_str)
             multiplicator = int(multiplicator_str)
             result += multiplicand * multiplicator
 
     print(f"{result=}")
+    assert result == 82857512
 
     return 0
 
